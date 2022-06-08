1931d0ea10f92f29291fa4eb6056c8450207a040
Angel Santos <iiisantosjangel@gmail.com>
2022-06-07 20:08:54 -0700

    added carousel

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
-const helpers = require('./utils/helpers');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -35,5 +35,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(3001, () => console.log('Now listening'));
 });
