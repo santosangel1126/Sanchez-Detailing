7cd900ad27798c47c70747e68aac4f4400cf12dd
Rush <kriscookies20@gmail.com>
2022-06-11 10:42:49 -0700

    login error solved

--- authentication/controllers/user.js
+++ authentication/controllers/user.js
@@ -1,9 +1,17 @@
 // In controllers/user.js
 // const uuid = require('uuid');
 const { v4: uuidv4 } = require('uuid');
-
 const Users = require('../config/dbUsers.json');
 const { hashPassword, checkPassword } = require('../utilities/passwordService');
+const { createToken, isValidToken } = require('../utilities/tokenService');
+
+const cookieOptions = {
+  expires: new Date(Date.now() + 900000),
+  httpOnly: true,
+  // secure: true, on deployment for https
+  signed: true
+};
+
 
 // import { v4 as uuidv4 } from 'uuid';
 // uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
@@ -24,6 +32,11 @@ const signup = async (req, res) => {
 };
 
 const getUsers = async (req, res) => {
+  try {
+    res.json(Users);
+  } catch (err) {
+    if (err) throw err;
+  }
     res.json(Users);
   };
 
@@ -33,17 +46,19 @@ const getUsers = async (req, res) => {
       let isMatch = await checkPassword(req.body.password, user.hashedPassword);
       if (user) {
         if (isMatch) {
-          res.status(200).redirect('/users/authorized');
-        } else {
-          res.send('sorry password did not match');
-        }
+          let token = await createToken(user);
+        console.log('token', token);
+        res.cookie('token', token, cookieOptions).redirect('/users/authorized');
       } else {
-        res.send('sorry username does not match');
+        res.send('sorry password did not match');
       }
-    } catch (err) {
-      if (err) throw err;
+    } else {
+      res.send('sorry username does not match');
     }
-  };
+  } catch (err) {
+    if (err) throw err;
+  }
+};
   
   const logout = async (req, res) => {
     res.send('you hit the logout in route');
