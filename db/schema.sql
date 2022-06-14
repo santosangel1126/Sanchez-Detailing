d2d3dfd6aad806dc235200f4bb42161de16895f3
Angel Santos <iiisantosjangel@gmail.com>
2022-06-14 16:12:50 -0700

    created table

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,9 @@
-DROP DATABASE IF EXISTS sanchez_detailing_db;
+DROP DATABASE IF EXISTS sanchez_clients;
 
-CREATE DATABASE sanchez_detailing_db;
+CREATE TABLE sanchez_clients ( 
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    fist_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    car_packages BOOLEAN NOT NULL,
+    appointment BOOLEAN(0,1,2,3) NOT NULL 
+);
---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO candidates (first_name, last_name, industry_connected)
+VALUES
+  ('Ronald', 'Firbank', 1, 0),
+  ('Virginia', 'Woolf', 1, 2),
+  ('Piers', 'Gaveston', 0, 1),
+  ('Charles', 'LeRoi', 1, 3),
+  ('Katherine', 'Mansfield', 1, 3),
+  ('Dora', 'Carrington', 0, 0),
+  ('Edward', 'Bellamy', 0, 1),
+  ('Montague', 'Summers', 1, 2),
+  ('Octavia', 'Butler', 1, 3),
+  ('Unica', 'Zurn', 1, 1);
