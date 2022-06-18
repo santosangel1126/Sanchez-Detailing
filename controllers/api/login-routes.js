f80c7ad68f80ca31daf024596164c3cf89e70861
bm <marcusb733@gmail.com>
2022-06-17 19:43:22 -0700

    Add User Routes

--- controllers/api/login-routes.js
+++ controllers/api/login-routes.js
@@ -4,6 +4,87 @@ const { User } = require('../../models');
 // get all users
 router.get('/', (req, res) => {
     User.findAll({
-        attributes
+        attributes: { exclude: ['password'] }
     })
-})
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// login route
+router.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+            username: req.body.email
+        }
+    })
+    .then(dbUserData => {
+        // if username is not found
+        if (!dbUserData) {
+            res.status(400).json({
+                message: 'No user with that username found!'
+            });
+            return;
+        }
+
+        const validPassword = dbUserData.checkPassword(req.body.password);
+
+        //if password is not correct
+        if (!validPassword) {
+            res.status(400).json({ 
+                message: 'Incorrect Password!'
+            });
+            return;
+        }
+
+        //save to session
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json({ 
+                user: dbUserData,
+                message: 'You are now logged in!'
+            });
+        });
+    });
+});
+
+// logout route
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
+
+// Update Route
+router.put('/:id', (req, res) => {
+    User.update(req.body, {
+        individualHooks: true,
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({
+                message: 'No User Found With This ID!'
+            });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
